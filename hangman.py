cc62d7601bb3bd7e50e6016e2526ee9b7f09ddb1
Coder-stuff <56047171+Coder-stuff@users.noreply.github.com>
2019-10-24 13:04:08 -0700

    Hangman v2
    Not finished, but now it prints the frames and lives and you can guess letters in the word.

--- hangman.py
+++ hangman.py
@@ -8,19 +8,7 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|                   \\|   |
 |                     |   |
-|                     |   |
-|                     |   |
-|                     |   |
-|                     |   |
-|                   ===== |
-|_________________________|''',
-'''
- _________________________
-|                         |
-|              +======+   |
-|             ( )   \\|   |
 |                     |   |
 |                     |   |
 |                     |   |
@@ -32,7 +20,19 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|             ( )   \\|   |
+|             ( )    \\|   |
+|                     |   |
+|                     |   |
+|                     |   |
+|                     |   |
+|                     |   |
+|                   ===== |
+|_________________________|''',
+'''
+ _________________________
+|                         |
+|              +======+   |
+|             ( )    \\|   |
 |              |      |   |
 |              |      |   |
 |              |      |   |
@@ -44,7 +44,7 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|             ( )   \\|   |
+|             ( )    \\|   |
 |              |      |   |
 |              |      |   |
 |              |      |   |
@@ -56,11 +56,11 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|             ( )   \\|   |
+|             ( )    \\|   |
 |              |      |   |
 |              |      |   |
 |              |      |   |
-|             /\\     |   |
+|             /\\      |   |
 |                     |   |
 |                   ===== |
 |_________________________|''',
@@ -68,11 +68,11 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|             ( )   \\|   |
+|             ( )    \\|   |
 |              |      |   |
 |             /|      |   |
 |              |      |   |
-|             /\\     |   |
+|             /\\      |   |
 |                     |   |
 |                   ===== |
 |_________________________|''',
@@ -80,11 +80,11 @@ frameList = [
  _________________________
 |                         |
 |              +======+   |
-|             ( )   \\|   |
+|             ( )    \\|   |
 |              |      |   |
 |             /|\\    |   |
 |              |      |   |
-|             /\\     |   |
+|             /\\      |   |
 |                     |   |
 |                   ===== |
 |_________________________|'''
@@ -146,7 +146,7 @@ else:
 
 x=0
 
-lives = 6
+lives = 0
 word = random.choice(wordList)
 print(word)	
 guessList = []
@@ -159,62 +159,26 @@ while True:
 	print("Here are the lettters that you have guessed so far: " + str(letterBank))
 	print("type (quit) to quit")
 	choice = input("Guess a letter: ")
+	index = 0
 	if choice == "quit":
 		break
-	if choice == a:
-		letterBank.append(a)
-	if choice == b:
-		letterBank.append(b)
-	if choice == c:
-		letterBank.append(c)
-	if choice == d:
-		letterBank.append(d)
-	if choice == e:
-		letterBank.append(e)
-	if choice == f:
-		letterBank.append(f)
-	if choice == g:
-		letterBank.append(g)
-	if choice == h:
-		letterBank.append(h)
-	if choice == i:
-		letterBank.append(i)
-	if choice == j:
-		letterBank.append(j)
-	if choice == k:
-		letterBank.append(k)
-	if choice == l:
-		letterBank.append(l)
-	if choice == m:
-		letterBank.append(m)
-	if choice == n:
-		letterBank.append(n)
-	if choice == o:
-		letterBank.append(o)
-	if choice == p:
-		letterBank.append(p)
-	if choice == q:
-		letterBank.append(q)
-	if choice == r:
-		letterBank.append(r)
-	if choice == s:
-		letterBank.append(s)
-	if choice == t:
-		letterBank.append(t)
-	if choice == u:
-		letterBank.append(u)
-	if choice == v:
-		letterBank.append(v)
-	if choice == w:
-		letterBank.append(w)
-	if choice == x:
-		letterBank.append(x)
-	if choice == y:
-		letterBank.append(y)
-	if choice == z:
-		letterBank.append(z)
-	lives=lives-1
-	if lives<=1:
+	for letter in word:
+		if letter == choice:
+			guessList[index] = choice
+		index += 1
+	if letter != choice:
+		lives += 1
+		print(lives)
+	letterBank.append(choice)
+	if lives == 0:
+		print(frameList[0])
+	if lives == 1:
+		print(frameList[1])
+	if lives == 2:
+		print(frameList[2])
+	if lives == 3:
+		print(frameList[3])
+	if lives >= 6:
 		print("you lose, out of lives")
 		break
 
